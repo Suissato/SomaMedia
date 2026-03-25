b3ad9bb51bbcf0f659a93091363787be2c42bec9
Tossato <tossato.marcos@gmail.com>
2026-03-24 21:16:37 -0300

    Adicionado função de média

--- Program.cs
+++ Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Menu();
+    }
+
+    static void Menu()
+    {
+        Console.Clear();
+        Console.WriteLine("Digite a quantidade de números que deseja informar.");
+        int qntInfo = int.Parse(Console.ReadLine());
+
+        List<double> numeros = new List<double>();
+
+        for (var i = 0; i < qntInfo; i++)
+        {
+            Console.WriteLine($"Digite o {i + 1}° número.");
+            double numeroDigitado = double.Parse(Console.ReadLine());
+            numeros.Add(numeroDigitado);
+        }
+
+        double soma = 0;
+        foreach (var item in numeros)
+        {
+            soma += item;
+        }
+
+        Console.WriteLine($"Média é igual a: {soma / qntInfo:f2}");
     }
 }
